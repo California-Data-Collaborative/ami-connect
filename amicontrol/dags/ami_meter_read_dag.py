688eb3c8000515d259a50bca0db74769f70f4a97
Matt Dowell <mattdowell12@gmail.com>
2025-04-08 14:16:19 -0700

    Add to DAG

--- amicontrol/dags/ami_meter_read_dag.py
+++ amicontrol/dags/ami_meter_read_dag.py
@@ -30,12 +30,26 @@ def ami_control_dag():
     @task()
     def transform(adapter: BaseAMIAdapter):
         adapter.transform()
+    
+    @task()
+    def load_raw(adapter: BaseAMIAdapter, config: AMIAdapterConfiguration):
+        adapter.load_raw(config)
+    
+    @task()
+    def load_transformed(adapter: BaseAMIAdapter, config: AMIAdapterConfiguration):
+        adapter.load_transformed(config)
 
     for adapter in adapters:
         extract.override(task_id=f"extract-{adapter.name()}")(adapter)
 
     for adapter in adapters:
         transform.override(task_id=f"transform-{adapter.name()}")(adapter)
+    
+    for adapter in adapters:
+        load_raw.override(task_id=f"load-raw-{adapter.name()}")(adapter, config)
+    
+    for adapter in adapters:
+        load_transformed.override(task_id=f"load-transformed-{adapter.name()}")(adapter, config)
 
 
 ami_control_dag()
