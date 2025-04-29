dbf51be46ac2df2c341d691ade9c77126a372fa0
Matt Dowell <mattdowell12@gmail.com>
2025-04-28 18:46:35 -0700

    Fix test

--- test/amicontrol/test_dags.py
+++ test/amicontrol/test_dags.py
@@ -1,9 +1,17 @@
+from unittest.mock import patch
+
 from test.base_test_case import BaseTestCase
+
+
+CONFIG = "../fixtures/all-config.yaml"
+SECRETS = "../fixtures/all-secrets.yaml"
 
 
 class TestDagImport(BaseTestCase):
 
-    def test_dag_import(self):
+    @patch("amicontrol.dags.ami_meter_read_dag.find_config_yaml", return_value=CONFIG)
+    @patch("amicontrol.dags.ami_meter_read_dag.find_secrets_yaml", return_value=SECRETS)
+    def test_dag_import(self, mock_secrets, mock_config):
         """
         Fingers and toes check, tries to ensure DAG will parse.
         """
