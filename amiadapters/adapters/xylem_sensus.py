6ea743accb0e8571e370eb7c3256f9b0eb57461f
Matt Dowell <mattdowell12@gmail.com>
2025-10-29 16:51:47 -0700

    Can parse the CMEP file and do a mock extract, next is writing transform code

--- amiadapters/adapters/xylem_sensus.py
+++ amiadapters/adapters/xylem_sensus.py
@@ -18,27 +18,39 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
-class XylemSensusMeterAndRead:
-    AccountNumber: str
-    # MeterSN: str
-    # MTUID: str
-    # Port: str
-    # AccountType: str
-    # Address1: str
-    # City: str
-    # State: str
-    # Zip: str
-    # RawRead: str
-    # ScaledRead: str
-    # ReadingTime: str
-    # LocalTime: str
-    # Active: str
-    # Scalar: str
-    # MeterTypeID: str
-    # Vendor: str
-    # Model: str
-    # Description: str
-    # ReadInterval: str
+class XylemSensusRead:
+    """
+    Sensus single reading
+    """
+
+    time: str
+    code: str
+    quantity: str
+
+
+@dataclass
+class XylemSensusMeterAndReads:
+    """
+    Sensus CMEP-formatted record that includes meter info and hourly readings.
+
+    See MEPMD01 section of https://www.sce.com/sites/default/files/inline-files/14%2B-%2BCalifornia%2BMetering%2BExchange%2BProtocol%2B-%2BV4.1-022013_AA.pdf
+    """
+
+    record_type: str
+    record_version: str
+    sender_id: str
+    sender_customer_id: str
+    receiver_id: str
+    reciever_customer_id: str
+    time_stamp: str
+    meter_id: str
+    purpose: str
+    commodity: str
+    units: str
+    calculation_constant: str
+    interval: str
+    quantity: str
+    reads: list[XylemSensusRead]
 
 
 class XylemSensusAdapter(BaseAMIAdapter):
@@ -80,7 +92,15 @@ class XylemSensusAdapter(BaseAMIAdapter):
         run_id: str,
         extract_range_start: datetime,
         extract_range_end: datetime,
-    ):
+    ) -> ExtractOutput:
+
+        with open("/Users/matthewdowell/Desktop/south-tahoe.csv", "r") as f:
+            reader = csv.reader(f, delimiter=",")
+            result = []
+            for row in reader:
+                xylem_meter_and_reads = self._parse_cmep_row(row)
+                result.append(xylem_meter_and_reads)
+
         # logger.info(
         #     f"Connecting to Xylem Sensus SFTP for data between {extract_range_start} and {extract_range_end}"
         # )
@@ -108,62 +128,114 @@ class XylemSensusAdapter(BaseAMIAdapter):
         #     for f in downloaded_files:
         #         logger.info(f"Cleaning up downloaded file {f}")
         #         os.remove(f)
+        output = "\n".join(json.dumps(i, cls=DataclassJSONEncoder) for i in result)
+        return ExtractOutput({"meters_and_reads.json": output})
 
-        # return ExtractOutput({"meters_and_reads.json": output})
+    def _parse_cmep_row(self, row: list[str]) -> XylemSensusMeterAndReads:
+        """
+        Parses a single row in a CMEP-formatted file.
 
-        path = ""
+        See CMEP docs linked elsewhere in this file for explanation of protocol, which
+        puts all of a meter's reads on a single line.
+        """
+        if row[0] != "MEPMD01":
+            raise Exception(f"Unrecognized report format: {row[0]}")
 
-    def _download_meter_and_read_files_for_date_range(
-        self,
-        sftp: paramiko.SFTPClient,
-        extract_range_start: datetime,
-        extract_range_end: datetime,
-    ) -> List[str]:
-        downloaded_files = []
-        all_files_on_server = sftp.listdir(self.sftp_meter_and_reads_folder)
-        logger.info(f"Found {len(all_files_on_server)} total files on server")
-        files_to_download = files_for_date_range(
-            all_files_on_server, extract_range_start, extract_range_end
+        if len(row) < 15:
+            raise Exception(f"Row does not match MEPMD01 format: {row}")
+
+        # The 13th (and last non-reading item) in the row says how many readings will follow in that row
+        # Readings come in groups of three values. So if there are 5 readings, then 15 values will follow.
+        quantity_index = 13
+        number_of_reads = int(row[quantity_index]) if row[quantity_index] else 0
+        reads = []
+        for i in range(number_of_reads):
+            start_of_read = quantity_index + 1 + (i * 3)
+            date_time_text, code, quantity = (
+                row[start_of_read],
+                row[start_of_read + 1],
+                row[start_of_read + 2],
+            )
+            if not date_time_text:
+                # TODO this is a valid state according to protocol, need to calculate from row's base date time plus intervals
+                # TODO but maybe we handle it in the transform?
+                raise Exception(
+                    "No date time text for reading, which we do not support"
+                )
+            reads.append(
+                XylemSensusRead(time=date_time_text, code=code, quantity=quantity)
+            )
+
+        return XylemSensusMeterAndReads(
+            record_type=row[0],
+            record_version=row[1],
+            sender_id=row[2],
+            sender_customer_id=row[3],
+            receiver_id=row[4],
+            reciever_customer_id=row[5],
+            time_stamp=row[6],
+            meter_id=row[7],
+            purpose=row[8],
+            commodity=row[9],
+            units=row[10],
+            calculation_constant=row[11],
+            interval=row[12],
+            quantity=row[13],
+            reads=reads,
         )
-        if not files_to_download:
-            raise Exception(
-                f"No files found on server for range {extract_range_start} to {extract_range_end}"
-            )
-        os.makedirs(self.local_download_directory, exist_ok=True)
-        for file in files_to_download:
-            local_csv = f"{self.local_download_directory}/{file}"
-            downloaded_files.append(local_csv)
-            logger.info(
-                f"Downloading {file} from FTP at {self.sftp_host} to {local_csv}"
-            )
-            sftp.get(self.sftp_meter_and_reads_folder + "/" + file, local_csv)
-        return downloaded_files
 
-    def _parse_downloaded_files(self, files: List[str]) -> Generator[str, None, None]:
-        for csv_file in files:
-            with open(csv_file, newline="", encoding="utf-8") as f:
-                csv_reader = csv.DictReader(f, delimiter=",")
-                for data in csv_reader:
-                    meter_and_read = XylemSensusMeterAndRead(**data)
-                    yield json.dumps(meter_and_read, cls=DataclassJSONEncoder)
+    # def _download_meter_and_read_files_for_date_range(
+    #     self,
+    #     sftp: paramiko.SFTPClient,
+    #     extract_range_start: datetime,
+    #     extract_range_end: datetime,
+    # ) -> List[str]:
+    #     downloaded_files = []
+    #     all_files_on_server = sftp.listdir(self.sftp_meter_and_reads_folder)
+    #     logger.info(f"Found {len(all_files_on_server)} total files on server")
+    #     files_to_download = files_for_date_range(
+    #         all_files_on_server, extract_range_start, extract_range_end
+    #     )
+    #     if not files_to_download:
+    #         raise Exception(
+    #             f"No files found on server for range {extract_range_start} to {extract_range_end}"
+    #         )
+    #     os.makedirs(self.local_download_directory, exist_ok=True)
+    #     for file in files_to_download:
+    #         local_csv = f"{self.local_download_directory}/{file}"
+    #         downloaded_files.append(local_csv)
+    #         logger.info(
+    #             f"Downloading {file} from FTP at {self.sftp_host} to {local_csv}"
+    #         )
+    #         sftp.get(self.sftp_meter_and_reads_folder + "/" + file, local_csv)
+    #     return downloaded_files
+
+    # def _parse_downloaded_files(self, files: List[str]) -> Generator[str, None, None]:
+    #     for csv_file in files:
+    #         with open(csv_file, newline="", encoding="utf-8") as f:
+    #             csv_reader = csv.DictReader(f, delimiter=",")
+    #             for data in csv_reader:
+    #                 meter_and_read = XylemSensusMeterAndRead(**data)
+    #                 yield json.dumps(meter_and_read, cls=DataclassJSONEncoder)
 
     def _transform(self, run_id: str, extract_outputs: ExtractOutput):
-        raw_meters_with_reads = extract_outputs.load_from_file(
-            "meters_and_reads.json", XylemSensusMeterAndRead
-        )
-        return self._transform_meters_and_reads(raw_meters_with_reads)
+        raise Exception("Not implemented")
+        # raw_meters_with_reads = extract_outputs.load_from_file(
+        #     "meters_and_reads.json", XylemSensusMeterAndRead
+        # )
+        # return self._transform_meters_and_reads(raw_meters_with_reads)
 
-    def _transform_meters_and_reads(
-        self, raw_meters_with_reads: List[XylemSensusMeterAndRead]
-    ) -> Tuple[List[GeneralMeter], List[GeneralMeterRead]]:
-        transformed_meters_by_device_id = {}
-        transformed_reads_by_key = {}
+    # def _transform_meters_and_reads(
+    #     self, raw_meters_with_reads: List[XylemSensusMeterAndRead]
+    # ) -> Tuple[List[GeneralMeter], List[GeneralMeterRead]]:
+    #     transformed_meters_by_device_id = {}
+    #     transformed_reads_by_key = {}
 
-        # TODO
+    #     # TODO
 
-        return list(transformed_meters_by_device_id.values()), list(
-            transformed_reads_by_key.values()
-        )
+    #     return list(transformed_meters_by_device_id.values()), list(
+    #         transformed_reads_by_key.values()
+    #     )
 
 
 # TODO DRY this out from Aclara code if we're going to use it
