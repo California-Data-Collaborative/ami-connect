3bb9df08d74ff033773b2db39c0eb5508adf34ea
Matt Dowell <mattdowell12@gmail.com>
2026-03-31 17:44:46 -0700

    Infra fixes for Vanta; Bump dependencies for vulnerability fixes (#10)

--- amideploy/infrastructure/variables.tf
+++ amideploy/infrastructure/variables.tf
@@ -25,6 +25,15 @@ variable "ami_connect_airflow_server_instance_size" {
   type        = string
 }
 
+variable "ssh_ip_allowlist" {
+  description = "IP CIDR blocks that can SSH into our AWS resources. ex: [192.168.1.1/32]"
+  type        = list(string)
+  validation {
+    condition     = alltrue([for cidr in var.ssh_ip_allowlist : can(cidrnetmask(cidr))])
+    error_message = "All elements in ssh_ip_allowlist must be valid CIDR blocks, e.g. end in '/32'."
+  }
+}
+
 variable "ami_connect_s3_bucket_name" {
   description = "Name for S3 bucket used for intermediate task outputs. Must be a globally unique name, so include your org name, e.g. my-company-ami-connect-bucket."
   type        = string
